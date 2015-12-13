d63edcce123fe3d25d80d1f97b551fa1fb4a3852
Bingjing Feng <bifeng@syr.edu>
2015-12-13 16:58:30 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,17 +1,11 @@
-library(shiny)
-library(RCurl)
-
-my.url <- "https://docs.google.com/spreadsheets/d/17_j1MnMg8S49B5K6duBqZttbolPtJ77VQxl8pLtGKTQ/pub?output=csv"
-
-referrals.raw <- getURL( my.url, ssl.verifypeer=FALSE )
-
-dat.referrals <- read.csv( textConnection(referals.raw) )
-
-
 ui<-fluidPage(
-  headerPanel('CNY'),
-  selectInput("need", "Reason for Referral:", 
-              choices = unique( dat.referrals$Learner.Need) ),
-  plotOutput("line")
-)
+  titlePanel('Central New York Community Foundation CNY'),
+  
+  sidebarLayout(
+    sidebarPanel = (selectInput(inputId = "need", 
+                    label = "Reasons for Referral:", 
+                    choices = unique( dat.referrals$Learner.Need))),
+    mainPanel = ( plotOutput("line1"))
+    )
+  )
 
