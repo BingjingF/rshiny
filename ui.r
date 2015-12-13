f9f94add6370dbe8b78850136073b3bd5365513d
Bingjing Feng <bifeng@syr.edu>
2015-12-13 15:11:49 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,6 +1,17 @@
+library(shiny)
+library(RCurl)
+
+my.url <- "https://docs.google.com/spreadsheets/d/17_j1MnMg8S49B5K6duBqZttbolPtJ77VQxl8pLtGKTQ/pub?output=csv"
+
+referrals.raw <- getURL( my.url, ssl.verifypeer=FALSE )
+
+dat.referrals <- read.csv( textConnection(referals.raw) )
+
+
 ui<-fluidPage(
-  headerPanel('Main Reasons for Referrals'),
-  checkboxGroupInput("reasons", label="Choices",choices=c("1","2","3","4")),
-  plotOutput("hist")
+  headerPanel('CNY'),
+  selectInput("need", "Reason for Referral:", 
+              choices = unique( dat.referrals$Learner.Need) ),
+  plotOutput("line")
 )
 
