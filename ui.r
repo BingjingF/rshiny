cd2ca3c2d3c594cefa37fa11f2e90c940137bc6f
BenBond <palladio@gmail.com>
2015-12-14 15:01:42 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -33,13 +33,24 @@ shinyUI(fluidPage(
       conditionalPanel('input.dataset==="Referrals"',
                        selectInput(inputId = "referrals",
                                    label = "Agency:", 
-                                   choices = unique( dat.referrals$Referred.To)))
+                                   choices = unique( dat.referrals$Referred.To))),
+      conditionalPanel('input.dataset==="Relationships"',
+                       checkboxGroupInput('show_refs', 
+                                          'Referral sources to show:', 
+                                          levels(dat.referrals$Referred.By.1), 
+                                          selected = levels(dat.referrals$Referred.By.1)),
+                       checkboxGroupInput('show_agencies', 
+                                          'Referred agencies to show:', 
+                                          levels(dat.referrals$Referred.To), 
+                                          selected = levels(dat.referrals$Referred.To)))
       ),
     
     mainPanel = (tabsetPanel(id='dataset',
                              tabPanel("Reasons",plotOutput("line1")),
                 
-                             tabPanel("Referrals",plotOutput("line"))
+                             tabPanel("Referrals",plotOutput("line")),
+                             
+                             tabPanel("Relationships", plotOutput("relationship"))
                              )
     ))
   ))
