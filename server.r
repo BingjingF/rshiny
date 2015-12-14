d783f70983673d33f8f0bfdb9089ad3c77b65802
Bingjing Feng <bifeng@syr.edu>
2015-12-14 15:33:39 -0500

    Update server.r

--- server.r
+++ server.r
@@ -54,7 +54,7 @@ shinyServer(function(input,output){
   dat.referrals.subset <- NA
   dat.referrals.subset <- dat.referrals[dat.referrals$Referred.By.1 == input$show_refs, ]
   counts <- table(dat.referrals.subset$Referred.To, dat.referrals.subset$Referred.By.1)
-  barplot(counts, xlim=c(0,6), ylim = c(0,30), main="Relationship Between Referral Source and Referral Agency",
+  barplot(counts, xlim=c(0,6), ylim = c(0,15), main="Relationship Between Referral Source and Referral Agency",
       xlab="Referral Sources", col=c("darkblue","darkorange1", "gray", "mediumorchid3", "red", "wheat1", "lawngreen"))
   })
   })
