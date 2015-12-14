cc76fe1358ef1e2c379a492e97395f132a0557c4
BenBond <palladio@gmail.com>
2015-12-14 15:10:42 -0500

    Update server.r

--- server.r
+++ server.r
@@ -7,6 +7,18 @@ referrals.raw <- getURL( my.url, ssl.verifypeer=FALSE )
 #data name: dat.referrals
 dat.referrals <- read.csv( textConnection(referrals.raw), stringsAsFactors=F )
 
+#find out data type
+lapply( dat.referrals, class )
+
+#change date to month
+date <- as.Date(dat.referrals$Timestamp, "%m/%d/%Y")
+
+month <- format( date, "%b")
+
+# you need to do this to ensure months are ordered correctly, default is alphabetic
+
+month <- factor( month, 
+                 levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
 
 shinyServer(function(input,output){
   
